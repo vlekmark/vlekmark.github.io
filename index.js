897024d3fbc9151ead9b9de44a9bcdb306e1f3f1
Mark Vlek <m.vlek@MoodMediaNL.local>
2021-03-02 15:41:26 +0100

    dsa

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 AFRAME.registerComponent('change-material-on-click', {
-	schema: {
+	/*schema: {
 		target: { type: 'selector' }
-	},
+	},*/
 	init: function () {
-		console.log(this.el)
-		console.log(this.data)
-		console.log(this.data.target)
-		this.data.target.addEventListener('model-loaded', () => this.update());
+		//console.log(this.el)
+		//console.log(this.data)
+		//console.log(this.data.target)
+		this.el.addEventListener('model-loaded', () => this.update());
 
 		/*el.addEventListener('click', () => {
 
 		});*/
 	},
 	update: function () {
-		let targetEl = this.data.target;  // Target to change material.
-		const mesh = targetEl.getObject3D('mesh');
-		mesh.traverse(node => {
+		//let targetEl = this.data.target;  // Target to change material.
+		const object = this.el.getObject3D('mesh');
+		object.traverse(node => {
+			console.log(node);
 			if (node.isMesh) {
 				node.material = targetEl.material;
 				console.log(targetEl.material)
