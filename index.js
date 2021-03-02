806ef5c0915506d2e665faa2a8b5bc48db67ede7
Mark Vlek <m.vlek@MoodMediaNL.local>
2021-03-02 15:54:32 +0100

    new logs

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 AFRAME.registerComponent('change-material', {
 	init: function () {
+		console.log('init')
 		this.el.addEventListener('model-loaded', () => {
+			console.log('model loaded')
 			const obj = this.el.getObject3D('mesh');
+			console.log(obj)
 			if (!obj) return;
 
 			obj.traverse(node => {
