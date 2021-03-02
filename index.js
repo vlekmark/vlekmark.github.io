331b0cfe79a6cc3f11b3b23747a3d4395002c950
Mark Vlek <m.vlek@MoodMediaNL.local>
2021-03-02 15:26:09 +0100

    change

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ AFRAME.registerComponent('change-material-on-click', {
 		target: { type: 'selector' }
 	},
 	init: function () {
-		let el = this.el;  // Element to add click listener.
-		el.addEventListener('model-loaded', () => this.update());
+		let targetEl = this.data.target;  // Target to change material.
+		targetEl.addEventListener('model-loaded', () => this.update(targetEl));
 
 		/*el.addEventListener('click', () => {
 
 		});*/
 	},
-	update: function () {
-		let targetEl = this.data.target;  // Target to change material.
+	update: function (targetEl) {
 		let mesh = targetEl.getObject3D('mesh');
 		console.log(targetEl)
-		console.log(this.data)
+		console.log(mesh)
 		mesh.traverse(node => {
 			if (node.isMesh) {
 				node.material = targetEl.material;
