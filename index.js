20a1aa5840574038c5de4572c77d0a422e2d555c
Mark Vlek <m.vlek@MoodMediaNL.local>
2021-03-02 14:34:11 +0100

    dhh

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ AFRAME.registerComponent('change-material-on-click', {
 		let targetEl = this.data.target;  // Target to change material.
 		console.log(targetEl)
 		el.addEventListener('click', function () {
+			console.log('further')
 			let mesh = targetEl.getObject3D('mesh');
+			console.log(mesh)
 			mesh.traverse(node => {
+				console.log(node)
 				if (node.name === "Shoe") {
 					console.log(node)
 				};
