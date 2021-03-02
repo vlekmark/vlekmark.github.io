e964fbe4a50ca37a223bfbaeca39e4f3c91545f1
Mark Vlek <m.vlek@MoodMediaNL.local>
2021-03-02 13:48:51 +0100

    things

--- index.js
+++ index.js
@@ -4,25 +4,26 @@ AFRAME.registerComponent('modify-materials', {
 			const obj = this.el.getObject3D('mesh');
 			// Go over the submeshes and modify materials we want.
 			obj.traverse(node => {
-				console.log(node);
-				
-
-				if (node.name.trim() == "Shoe") {
+				if (node.name === "Shoe") {
+					console.log(node.material);
 					try {
-						console.log(node.primitives[0].material);
-						node.primitives[0].material = 1;
+						node.material.set(1);
 					} catch {
-						(e => console.warn(e));
+						(e => console.warn('cant do that'+ e));
 					};
+
 					try {
-						node.primitives[0].material.set(1);
+						node.material = 1;
 					} catch {
-						(e => console.warn(e));
-					}
+						(e => console.warn('cant do that' + e));
+					};
+
+					try {
+						node.material.color.set('red');
+					} catch {
+						(e => console.warn('cant do that' + e));
+					};
 				}
-				/*if (node.name.indexOf('ship') !== -1) {
-					node.material.color.set('red');
-				}*/
 			});
 		});
     }
