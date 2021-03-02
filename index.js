eee9dd68c8109aed0c2055af84a6376fffc26602
Mark Vlek <m.vlek@MoodMediaNL.local>
2021-03-02 15:16:22 +0100

    new model target

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ AFRAME.registerComponent('change-material-on-click', {
 	},
 	init: function () {
 		let el = this.el;  // Element to add click listener.
-		console.log('further')
 		el.addEventListener('model-loaded', () => this.update());
 
 		/*el.addEventListener('click', () => {
@@ -13,11 +12,8 @@ AFRAME.registerComponent('change-material-on-click', {
 	},
 	update: function () {
 		let targetEl = this.data.target;  // Target to change material.
-		console.log(targetEl);
-		console.log('further')
 		let mesh = targetEl.getObject3D('mesh');
 		console.log(mesh)
-		console.log('further')
 		mesh.traverse(node => {
 			if (node.isMesh) {
 				node.material = targetEl.material;
