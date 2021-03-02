8a6c850efe633a8a1a74ef4baa23a49a5c12dd5e
Mark Vlek <m.vlek@MoodMediaNL.local>
2021-03-02 16:02:22 +0100

    set mat

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 AFRAME.registerComponent('change-material', {
 	init: function () {
-		console.log('init')
 		this.el.addEventListener('model-loaded', () => {
-			console.log('model loaded')
 			const obj = this.el.getObject3D('mesh');
-			console.log(obj)
 			if (!obj) return;
 
 			obj.traverse(node => {
-				console.log(node);
 				if (node.isMesh) {
-					//node.material = targetEl.material;
-					console.log(node)
+					node.material = this.material;
+					console.log(this.material)
 				};
 			});
 		});
