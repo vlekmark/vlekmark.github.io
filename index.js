02b45c420bce3364162d3e523098b7ed9d9e41e2
Mark Vlek <m.vlek@MoodMediaNL.local>
2021-03-02 13:22:15 +0100

    change

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ AFRAME.registerComponent('modify-materials', {
 			// Go over the submeshes and modify materials we want.
 			obj.traverse(node => {
 				console.log(node.name);
-				console.log(node.primitives[0].material);
+				
 
-				try {
-					node.primitives[0].material = 1;
-				} catch {
-					(e => console.warn(e));
-				};
-				try {
-					node.primitives[0].material.set(1);
-				} catch {
-					(e => console.warn(e));
+				if (node.name === "shoe") {
+					console.log(node.primitives[0].material);
+
+					try {
+						node.primitives[0].material = 1;
+					} catch {
+						(e => console.warn(e));
+					};
+					try {
+						node.primitives[0].material.set(1);
+					} catch {
+						(e => console.warn(e));
+					}
 				}
 				/*if (node.name.indexOf('ship') !== -1) {
 					node.material.color.set('red');
